f57ac254d3be25b478d6b3bafc87d7ca9d482d5d
Moksh goyal <mokshgoyal26@gmail.com>
2026-01-31 01:31:52 +0530

    feat: implement smooth dragable sidebar using transform and RAF  - Enable dragging via sidebar header  - used transform based movement for smooth performance  - prevent sidebar moving from top viewport

--- Extension/content-scripts/script.js
+++ Extension/content-scripts/script.js
@@ -114,4 +114,97 @@ fetch(chrome.runtime.getURL('content-scripts/sidebar.html'))
 
         sidebarLogo.style.setProperty('background-color', 'red');*/
 
+        const sidebar = document.querySelector('.sidebar-extension-container');
+        const header = document.querySelector('.sidebar-header-container');
+
+        console.log('sidebar : ',sidebar);
+        console.log('header : ',header);
+
+        initDrag(sidebar,header);
+
     });
+
+
+    function initDrag(sidebar,header){
+            
+
+        //console.log('sidebar',sidebar);
+        //console.log('header',header);
+
+        let is_dragging = false;
+
+        let startMouseX = 0;
+        let startMouseY = 0;
+
+        let startSidebarX = 0;
+        let startSidebarY = 0;
+
+        let currentX = 0;
+        let currentY = 0;
+
+        let rafId = null;
+
+        header.addEventListener('mousedown' , (e) => {
+            is_dragging = true;
+
+            startMouseX = e.clientX;
+            startMouseY = e.clientY;
+
+            /*const rect = sidebar.getBoundingClientRect();
+            startSidebarX = rect.left;
+            startSidebarY = rect.top;
+
+            sidebar.style.left = `${startSidebarX}px`;
+            sidebar.style.top = `${startSidebarY}px`;
+            sidebar.style.right = 'auto';*/
+
+            sidebar.style.willChange = 'transform';
+            document.body.style.userSelect = 'none';
+        });
+
+        // moving sidebar while dragging 
+
+        document.addEventListener('mousemove',(e) =>{
+            if(!is_dragging) return;
+
+            const deltaX = e.clientX - startMouseX;
+            const deltaY = e.clientY - startMouseY;
+
+            currentX += deltaX;
+            currentY += deltaY;
+
+            // prevent sidebar going out of viewport from the top
+            if(currentY < 0) currentY = 0;
+
+            if(!rafId){
+                rafId = requestAnimationFrame(updatePosition);
+            }
+
+            /*sidebar.style.left = `${startSidebarX + deltaX}px`;
+            sidebar.style.top = `${startSidebarY + deltaY}px`;
+            sidebar.style.right = 'auto';*/
+
+            startMouseX  = e.clientX;
+            startMouseY = e.clientY;
+
+
+        });
+
+        // stop the moving sidebar 
+
+        document.addEventListener('mouseup', () =>{
+                is_dragging = false;
+
+                sidebar.style.willChange = '';
+                document.body.style.userSelect = '';
+
+                cancelAnimationFrame(rafId);
+                rafId = null;
+        });
+
+
+        function updatePosition(){
+            sidebar.style.transform = `translate(${currentX}px , ${currentY}px)`;
+            rafId = null;
+        }
+    }
