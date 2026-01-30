3ebf2f66197513e8dd935c3765b5631f594b1b37
Moksh goyal <mokshgoyal26@gmail.com>
2026-01-30 21:01:18 +0530

    chore: add addListener to recieve page data from content-script

--- Extension/service-worker/background.js
+++ Extension/service-worker/background.js
@@ -1,1 +1,7 @@
+chrome.runtime.onMessage.addListener((message,sender,response) =>{
+    if(message.type === 'Page_Context_Ready'){
+        console.log('Recieved data from the page : ',message.payload);
+    }
+});
+
 console.log('hello');
