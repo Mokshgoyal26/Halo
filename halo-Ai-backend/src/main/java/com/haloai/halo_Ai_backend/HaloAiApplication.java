100adc0ac88d4cc5781b95c0adf936078e8e6428
Moksh goyal <mokshgoyal26@gmail.com>
2026-02-07 11:34:00 +0530

    chore: added backend in spring boot framework to project

---
+++ halo-Ai-backend/src/main/java/com/haloai/halo_Ai_backend/HaloAiApplication.java
@@ -0,0 +1,14 @@
+package com.haloai.halo_Ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloAiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HaloAiApplication.class, args);
+	}
+
+}
---
+++ halo-Ai-backend/src/test/java/com/haloai/halo_Ai_backend/HaloAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haloai.halo_Ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
