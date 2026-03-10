19c8644d5278e2ee86bd9e13ed2a73d8e4b7a020
Moksh goyal <mokshgoyal26@gmail.com>
2026-03-11 03:12:02 +0530

    feat(refactor) : Refactored Authentication Ui -> seprate login and signup forms - split authentication into 2 seprate forms on the same page for better UX - fix input styling issues (default behavior) - refactored whole html structure added complete new structure compatatible with both forms - changed complete css file added new styling to the authentication - added glow stick to the top of authentication page - added box shadow to the submit button - added dragging logic to the authentication page (now authentication page is complete draggable) - changed user input for signup form now it is accepting username , email and password and handles it at signup.js file

--- Extension/ui/sign-up-page-ui/signUp.js
+++ Extension/ui/sign-up-page-ui/signUp.js
@@ -6,7 +6,7 @@ export async function  getSignUpPage(shadow){
 
 async function mountSignUpPage(shadow){
 
-    let signupPageElement = shadow.querySelector('.signup-page');
+    let signupPageElement = shadow.querySelector('.auth-page');
 
     if(!signupPageElement){
 
@@ -31,7 +31,7 @@ async function mountSignUpPage(shadow){
         shadow.appendChild(style);
 
 
-        signupPageElement = shadow.querySelector('.signup-page');
+        signupPageElement = shadow.querySelector('.auth-page');
         console.log('signup-page existed: ', signupPageElement);
         
     }else{
@@ -44,6 +44,8 @@ async function mountSignUpPage(shadow){
     closeButtonFeature(signupPageElement);
     handleSignupRequest(signupPageElement);
     handleLoginRequest(signupPageElement);
+    handleAuthFormToggle(signupPageElement);
+    handleAuthPageDrag(signupPageElement);
     
 
 }
@@ -51,7 +53,7 @@ async function mountSignUpPage(shadow){
 
 function closeButtonFeature(signupPage){
 
-    const closebtn = signupPage.querySelector('.signup-close-btn');
+    const closebtn = signupPage.querySelector('.auth-close-btn');
 
     if(!closebtn){
         console.warn('signupPage and closebtn elements not exists yet');
@@ -66,26 +68,29 @@ function closeButtonFeature(signupPage){
 
 function handleSignupRequest(signupPage){
 
-    const userInput = signupPage.querySelector('#auth-email');
-    const passwordInput = signupPage.querySelector('#auth-password');
-    const signUpBtn = signupPage.querySelector('.auth-submit-btn');
+    const usernameInput = signupPage.querySelector('#signup-username');
+    const emailInput = signupPage.querySelector('#signup-email');
+    const passwordInput = signupPage.querySelector('#signup-password');
+    const signUpBtn = signupPage.querySelector('.signup-submit-btn');
 
-    console.log('userInput : ',userInput);
-    console.log('passwordInput : ',passwordInput);
+    console.log('username : ',usernameInput);
+    console.log('email : ',emailInput);
+    console.log('password : ',passwordInput);
     
 
-    if(!userInput || !passwordInput || !signUpBtn){
+    if(!usernameInput || !passwordInput || !emailInput || !signUpBtn){
         console.warn('sign up inputs not found');
         return ;
     }
 
     signUpBtn.addEventListener('click' , async () =>{
         console.log('signupBtn clicked ... ');
-        const user = userInput.value;
+        const user = usernameInput.value;
+        const mail = emailInput.value; 
         const password = passwordInput.value;
 
-        if(!user || !password){
-            console.warn('user or password is empty');
+        if(!user || !password || !mail){
+            console.warn('user or password or mail is empty');
             return;
         }
 
@@ -93,6 +98,7 @@ function handleSignupRequest(signupPage){
             type: 'SIGN_UP_REQUEST',
             payload:{
                 user,
+                mail,
                 password
             }
         }, (response) =>{
@@ -106,22 +112,22 @@ function handleLoginRequest(signupPage){
 
     console.log('handlelogin function is initialized ... ');
 
-    const userInput = signupPage.querySelector('#auth-email');
-    const passwordInput = signupPage.querySelector('#auth-password');
-    const loginBtn = signupPage.querySelector('.login-btn');
+    const usernameInput = signupPage.querySelector('#login-username');
+    const passwordInput = signupPage.querySelector('#login-password');
+    const loginBtn = signupPage.querySelector('.login-submit-btn');
 
-    console.log('userInput : ',userInput);
+    console.log('userInput : ',usernameInput);
     console.log('passwordInput : ',passwordInput);
     
 
-    if(!userInput || !passwordInput || !loginBtn){
+    if(!usernameInput || !passwordInput || !loginBtn){
         console.warn('login inputs not found');
         return ;
     }
 
     loginBtn.addEventListener('click' , async () =>{
         console.log('login button  clicked ... ');
-        const user = userInput.value;
+        const user = usernameInput.value;
         const password = passwordInput.value;
 
         if(!user || !password){
@@ -138,6 +144,92 @@ function handleLoginRequest(signupPage){
         }, (response) =>{
             console.log('login Result : ', response);
         }); 
+    });
+}
+
+
+function handleAuthFormToggle(signupPage){
+
+    const loginForm = signupPage.querySelector('.login-form');
+    const signupForm = signupPage.querySelector('.signup-form');
+
+    const showLoginPageBtn = signupPage.querySelector('.show-login-page-btn');
+    const showSignupPageBtn = signupPage.querySelector('.show-signup-page-btn');
+
+
+    if(!loginForm || !signupForm){
+        console.warn('auth forms are missing');
+        return;
+    }
+
+    showLoginPageBtn?.addEventListener('click',() => {
+        
+            console.log('show login page btn is clicked .....');
+            loginForm.style.display = 'flex';
+            signupForm.style.display = 'none';
+    });
+
+
+    showSignupPageBtn?.addEventListener('click', () =>{
+
+        console.log('show signup page btn is clicked .....');
+        loginForm.style.display = 'none';
+        signupForm.style.display = 'flex';
+    });
+}
+
+
+function handleAuthPageDrag(signupPage){
+
+    const header = signupPage.querySelector('.auth-name-logo');
+
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    header.style.cursor = 'grab';
+
+    header.addEventListener('mousedown' , (e) =>{
+
+        isDragging = true;
+
+        const rect = getBoundingClientRect();
+        offsetX = e.clientX - rect.left;
+        offsetY = e.clientY - rect.top;
+
+        header.style.cursor = 'grabbing';
+        e.preventDefault();
+    });
+
+    document.addEventListener('mousemove' , (e) =>{
+
+        if(!isDragging){
+            return;
+        }
+
+        let newLeft = e.clientX - offsetX;
+        let newTop = e.clientY - offsetY;
+
+        // keeping it in within viewports 
+
+        const maxX  = window.innerWidth - signupPage.offsetWidth;
+        const maxY = window.innerHeight - signupPage.offsetHeight;
+
+        newLeft = Math.max(0 , Math.min(newLeft , maxX));
+        newTop = Math.max(0 , Math.min(newTop , maxY));
+
+
+        signupPage.style.right = 'auto';
+        signupPage.style.left = newLeft + 'px';
+        signupPage.style.top = newTop + 'px';
+    });
+
+    document.addEventListener('mouseup' , ()=>{
+
+        if(!isDragging) return;
+
+        isDragging = false;
+        header.style.cursor = 'grab';
     });
 }
 
